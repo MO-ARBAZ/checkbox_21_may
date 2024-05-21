4baf77f8b0da0f93313f37df8f0ddb1f6be5c42d
Mohd-Arbaj <arbaj@e-pspl.com>
2024-05-21 12:05:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+export default function App() {
+    const [checkedOption , setcheckedOption] =  useState([]);
 
-function App() {
+    const hanleCheckboxChange = (option) =>{
+            if(checkedOption.includes(option)){
+                setcheckedOption(checkedOption.filter((item) =>item!==option));
+                
+            }else{
+                setcheckedOption([...checkedOption , option]);
+            }
+    }
+    console.log("Hi")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+        <h1>Checkedlinst</h1>
+      <form>
 
-export default App;
+        <label>
+        
+          <input
+          type ="checkbox"
+          value = "Option 1"
+          onChange ={() =>
+          hanleCheckboxChange("Option 1")}
+          checked={checkedOption.includes("Option 1")}
+      />
+            Option 1
+         </label>
+            
+            <br/>
+
+            <label>
+          <input
+          type ="checkbox"
+          value = "Option 2"
+          onChange ={() =>
+          hanleCheckboxChange("Option 2")}
+          checked={checkedOption.includes("Option 2")}
+      />
+          Option 2
+          
+        </label>
+            <br/>
+            <label>
+          <input
+          type ="checkbox"
+          value = "Option 3"
+          onChange ={() =>
+          hanleCheckboxChange("Option 3")}
+          checked={checkedOption.includes("Option 3")}
+      />
+          Option 3
+     
+        </label>
+              
+      </form>
+      <h3>Selected Option </h3>
+          <ul>
+
+            {checkedOption.map((option, index) =>
+            (
+            <li key={index}>{option}</li>
+            ))}
+          </ul>
+      </div>
+  )
+}
